67f1f554ddba3b61181ea19d3d1f253f7d116031
alokaku04 <31933728+alokaku04@users.noreply.github.com>
2017-09-24 11:38:57 -0300

    Add files via upload

---
+++ day1task6.py
@@ -0,0 +1,20 @@
+def grid(row, column):
+        for rows in range(0, row):
+                for columns in range(0, column):
+                        print ('+', end='')
+                        for z in range(0, 4):
+                                print ('-', end='')
+                print ('+')
+                for y in range(0, 4):
+                        for columns in range(0, column):
+                                print ('/', end='')
+                                for z in range(0, 4):
+                                        print (' ', end='')
+                        print ('/')
+        for columns in range(0, column):
+                        print ('+', end='')
+                        for z in range(0, 4):
+                                print ('-', end='')
+        print ('+')
+grid(2, 2)
+grid(4, 4)
